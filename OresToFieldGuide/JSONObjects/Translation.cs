8430f3f4dc837754bb3f73f6772e10aa8d18c2f0
Pyritie <pyritie@gmail.com>
2026-04-17 14:21:26 +0100

    moved the tool over to use proper locIDs

--- OresToFieldGuide/JSONObjects/Translation.cs
+++ OresToFieldGuide/JSONObjects/Translation.cs
@@ -4,12 +4,6 @@ namespace OresToFieldGuide
 {
 	public class Translation
 	{
-		[JsonPropertyName("lang")]
-		public required string Language { get; set; }
-
-		[JsonPropertyName("text")]
-		public required string Text { get; set; }
-
 		[JsonPropertyName("info")]
 		public string? Info { get; set; }
 
